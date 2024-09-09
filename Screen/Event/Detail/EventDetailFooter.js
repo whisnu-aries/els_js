495922e7741687d487811c6af9fa4e8bc3731659
Whisnu <magran.whisnu@gmail.com>
2024-09-09 22:48:44 +0700

    fix (components) - fix import component

--- Screen/Event/Detail/EventDetailFooter.js
+++ Screen/Event/Detail/EventDetailFooter.js
@@ -1,20 +1,26 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 import Styles from "./EventDetailFooter.Style";
 import { Colors } from "../../../Constants/Colors";
 
-import PrimaryButton from "../../../Component/Button/PrimaryButton";
+import GeneralButton from "../../../Component/Button/GeneralButton";
 import SafeAreaFooter from "../../../Component/SafeArea/SafeAreaFooter";
 
 const handlePrimaryButton = () => {
   console.log("a");
 };
 
-const EventDetailFooter = () => (
-  <View style={Styles.container}>
-    <PrimaryButton text="Register" onPress={handlePrimaryButton} />
-    <SafeAreaFooter color={Colors.slate} />
-  </View>
-);
+const EventDetailFooter = () => {
+  return (
+    <View style={Styles.container}>
+      <GeneralButton
+        type="primary"
+        text="Register"
+        onPress={handlePrimaryButton}
+      />
+      <SafeAreaFooter color={Colors.slate} />
+    </View>
+  );
+};
 
 export default EventDetailFooter;
